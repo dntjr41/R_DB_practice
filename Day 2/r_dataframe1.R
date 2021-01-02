28137eea6be25b94c1253b28f6d5ea48e86b0b33
dntjr41 <67234937+dntjr41@users.noreply.github.com>
2021-01-02 10:55:00 +0900

    Add files via upload

---
+++ Day 2/r_dataframe1.R
@@ -0,0 +1,18 @@
+df<-data.frame(name=c("조정순", "한민애", "고승민"), age=c(43,27,33))
+df
+
+name<-c("조정순", "한민애", "고승민")
+age<-c(43, 27, 33)
+df2<-data.frame(이름=name, 나이=age)
+df2
+
+df2$이름
+df2$나이+1
+
+carinfo<-matrix(c('11km','13km','15km','h사','p사','r사'), ncol=2)
+carinfo
+dfr<-as.data.frame(carinfo)
+dfr
+
+names(dfr)<-c("연비", "제조사")
+dfr
